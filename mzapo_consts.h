6abfa079ac3ac1720886899537043bef6eeb698a
R.CH <chladrad@fel.cvut.cz>
2021-04-28 18:34:10 +0200

    DEV: file for our used constants

---
+++ mzapo_consts.h
@@ -0,0 +1,15 @@
+/**
+  mzapo_consts.h      -constants we are using
+
+  (C) Copyright 2021 by Lukáš Kořínek and Radek Chládek
+      e-mail:   korinlu1@fel.cvut.cz chladrad@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ **/
+
+//mainly for mzapo_led_functions
+#define RED 0xff0000
+#define GREEN 0x00ff00
+#define BLUE 0x0000ff
+
+
