72180ea571bb0fac539a3f641676f6c23ffa683b
Lukáš Kořínek <korinlu1@fel.cvut.cz>
2021-05-18 17:59:45 +0200

    DEV: coding style

--- barrier.c
+++ barrier.c
@@ -16,17 +16,17 @@ barrier our_barriers[NUMBER_OF_BARRIERS];
 unsigned short colors[] = {0xf0f,0xfff,0xf0f,0xfff};
 void init_barriers()
 {
-    int x = 0, y = 0, posun = 15;
-    for (int i = 0; i < NUMBER_OF_BARRIERS; i++) {
+    int x = 0, y = 0, shift = 15;
+    for (int i = 0; i < NUMBER_OF_BARRIERS; ++i) {
         our_barriers[i].x = x;
         our_barriers[i].y = y;
         our_barriers[i].height = 25;
         our_barriers[i].width = 80;
         our_barriers[i].destroyed = false;
-        x += our_barriers[i].width + posun;
-        if (x >= (LCD_WIDTH - posun)) {
+        x += our_barriers[i].width + shift;
+        if (x >= (LCD_WIDTH - shift)) {
             x = 0;
-            y += 25 + posun;
+            y += 25 + shift;
         }
     }
 }
@@ -34,10 +34,10 @@ void init_barriers()
 void draw_barriers(unsigned char *parlcd_mem_base)
 {
     int color_number = 0;
-    for (int b = 0; b < NUMBER_OF_BARRIERS; b++) {
+    for (int b = 0; b < NUMBER_OF_BARRIERS; ++b) {
         if (b != 0 && b %5 == 0)
         {
-            color_number++;
+            ++color_number;
         }
         for (int i = 0; i < our_barriers[b].width; ++i) {
             for (int j = 0; j < our_barriers[b].height; ++j) {
@@ -49,7 +49,7 @@ void draw_barriers(unsigned char *parlcd_mem_base)
 
 void update_barriers(unsigned char *parlcd_mem_base) {
     int color_number = 0;
-    for (int b = 0; b < NUMBER_OF_BARRIERS; b++) {
+    for (int b = 0; b < NUMBER_OF_BARRIERS; ++b) {
         for (int i = 0; i < our_barriers[b].width; ++i) {
             for (int j = 0; j < our_barriers[b].height; ++j) {
                 if (!our_barriers[b].destroyed)
@@ -63,7 +63,7 @@ void update_barriers(unsigned char *parlcd_mem_base) {
         }
         if (b != 0 && b %5 == 0)
         {
-            color_number++;
+            ++color_number;
         }   
     }
 }
@@ -71,7 +71,7 @@ void update_barriers(unsigned char *parlcd_mem_base) {
 
 bool bounce_from_barriers(int x, int y, int ball_w , int ball_h,unsigned char *parlcd_mem_base) {
     bool ret = false;
-    for (int i = NUMBER_OF_BARRIERS; i >=0 ; i--)
+    for (int i = NUMBER_OF_BARRIERS; i >=0 ; --i)
     {
         if ((x+ball_w) > (our_barriers[i].x) && (our_barriers[i].x + our_barriers[i].width) > x && (y - ball_h) < (our_barriers[i].y + our_barriers[i].height) && !our_barriers[i].destroyed)
         {
